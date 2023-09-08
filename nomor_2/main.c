1969778a773d7d0943e8556fe636d426b6db975a
CobaKauPikirkan <fachrurozirizky@gmail.com>
2023-09-08 14:18:01 +0700

    change code logic for new requirement

--- nomor_2/main.c
+++ nomor_2/main.c
@@ -1,64 +1,62 @@
 #include <stdio.h>
 
-void main() {
-    double totalBelanja, diskon, hargaSetelahDiskon;
-    int jumlahTransaksi = 0;
+void main()
+{
+    double totalBelanjaBulanan = 0.0; // Total belanja dalam 1 bulan
     char lanjut;
+    int jumlahTransaksi = 0;
 
     printf("================================================\n");
     printf("Program penghitungan total belanja dalam 1 bulan\n");
     printf("=============== Fachrurozi Rizky ===============\n");
+    printf("================================================\n");
 
     do
     {
+        double totalBelanjaTransaksi;
+
         printf("Masukkan total belanja: Rp. ");
-        scanf("%lf", &totalBelanja);
+        scanf("%lf", &totalBelanjaTransaksi);
 
-        if (totalBelanja < 200000)
-        {
-            diskon = 0;
-        }
-        else if (totalBelanja >= 200000 && totalBelanja <= 500000)
-        {
-            diskon = 0.1; // Diskon 10%
-        }
-        else if (totalBelanja > 550000 && totalBelanja <= 1000000)
-        {
-            diskon = 0.2; // Diskon 20%
-        }
-        else
-        {
-            diskon = 0.3; // Diskon 30%
-        }
-
-        // Cek apakah jumlah transaksi mencapai 4 dalam sebulan
-        if (jumlahTransaksi >= 3)
-        {
-            diskon += 0.2; // penambahan diskon 20%
-        }
-
-        hargaSetelahDiskon = totalBelanja - (totalBelanja * diskon);
-        printf("Diskon yang diterapkan: %.0lf%%\n", diskon * 100);
-        printf("Total harga yang harus dibayar setelah diskon: Rp. %.2lf\n", hargaSetelahDiskon);
-
+        totalBelanjaBulanan += totalBelanjaTransaksi;
         jumlahTransaksi++;
 
-        if (jumlahTransaksi < 4)
-        {
-            printf("================================================\n");
-            printf("Apakah Anda ingin memasukkan transaksi lagi? (y/n): ");
-            scanf(" %c", &lanjut);
-            printf("================================================\n");
-        }
-        else
-        {
-            printf("================================================\n");
-            printf("Anda telah mencapai batas maksimum transaksi (4 transaksi).\n");
-            printf("================================================\n");
-            lanjut = 'n'; // Keluar dari loop
-        }
-
+        printf("================================================\n");
+        printf("Apakah Anda ingin memasukkan transaksi lagi? (y/n): ");
+        scanf(" %c", &lanjut);
+        printf("================================================\n");
 
     } while (lanjut == 'y' || lanjut == 'Y');
-    
+
+    double diskon = 0.0; // Diskon awalnya adalah 0%
+
+    if (totalBelanjaBulanan < 200000)
+    {
+        diskon = 0.0;
+    }
+    else if (totalBelanjaBulanan >= 200000 && totalBelanjaBulanan <= 500000)
+    {
+        diskon = 0.1; // Diskon 10%
+    }
+    else if (totalBelanjaBulanan > 500000 && totalBelanjaBulanan <= 1000000)
+    {
+        diskon = 0.2; // Diskon 20%
+    }
+    else
+    {
+        diskon = 0.3; // Diskon 30%
+    }
+
+    // Cek apakah total belanja dalam sebulan memenuhi syarat tambahan diskon
+    if (jumlahTransaksi >= 4)
+    {
+        printf("Anda telah mencapai total belanja bulanan yang memenuhi syarat tambahan diskon\n");
+        printf(">>>>>>> mendapat tambahan diskon 20%% <<<<<<<<\n");
+        diskon += 0.2; // Penambahan diskon 20%
+    }
+
+    double hargaSetelahDiskon = totalBelanjaBulanan - (totalBelanjaBulanan * diskon);
+    printf("Diskon yang diterapkan: %.0lf%%\n", diskon * 100);
+    printf("Total harga yang harus dibayar setelah diskon: Rp. %.2lf\n", hargaSetelahDiskon);
+    printf("================================================\n");
 }
